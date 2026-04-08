711d16dcfde2ef1ffa371650da6039456ddb5a9b
Lee Bradley <bradley_lee_r@sbcglobal.net>
2026-04-08 07:43:43 -0400

    Update problem_6c.py

--- problem_6c.py
+++ problem_6c.py
@@ -1,9 +1,11 @@
 
 # problem_6c.py - lrb - 4/2/26
 # initial program used AI
+# added problem 6c stuff, commented out not 6c stuff - 4/8/26
 
 import cmath
 
+'''
 z1 = complex(3, -7)
 r1 = abs(z1)
 theta1 = cmath.phase(z1)
@@ -31,4 +33,24 @@ r4 = abs(z4)
 theta4 = cmath.phase(z4)
 print(f"{r4=}")
 print(f"{theta4=}")
+'''
 
+z5 = complex(2, -7)
+r5 = abs(z5)
+theta5 = cmath.phase(z5)
+
+print(f"{r5=}")
+print(f"{theta5=}")
+
+z6 = complex(-3,4)
+r6 = abs(z6)
+theta6 = cmath.phase(z6)
+
+print(f"{r6=}")
+print(f"{theta6}")
+
+z7 = z5*z6
+r7 = abs(z7)
+theta7 = cmath.phase(z7)
+print(f"{r7=}")
+print(f"{theta7}")
